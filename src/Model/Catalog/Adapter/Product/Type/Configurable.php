469eecb7a7dcdcb1e79fe4c495efd84a885b4439
Danny Verkade <danny@cream.nl>
2019-08-06 15:02:50 -0700

    Changed handeling of configurable products to be able to handle multiple configurable products in one call to the catalog endpoint.

--- src/Model/Catalog/Adapter/Product/Type/Configurable.php
+++ src/Model/Catalog/Adapter/Product/Type/Configurable.php
@@ -5,7 +5,6 @@ namespace Gudtech\RetailOps\Model\Catalog\Adapter\Product\Type;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\ProductFactory;
 use Magento\Catalog\Model\ProductRepository;
-use Magento\ConfigurableProduct\Model\Product\Type\Configurable as ConfigurableProduct;
 use Magento\Eav\Model\AttributeRepository;
 use Magento\Eav\Model\Entity;
 use Magento\Eav\Model\ResourceModel\Entity\Attribute\Set as AttributeSet;
@@ -14,6 +13,14 @@ use Gudtech\RetailOps\Model\Catalog\Adapter\Product\Attribute as AttributeAdapte
 
 class Configurable extends Adapter
 {
+    /**
+     * @var string
+     */
+    const CONFIGURABLE_ATTRIBUTE_NAME = 'Configurable SKU';
+
+    /**
+     * @var array
+     */
     private $associations  = [];
 
     /**
@@ -21,11 +28,51 @@ class Configurable extends Adapter
      */
     private $configurableAttributes = [];
 
+    /**
+     * @var ProductRepository
+     */
     private $productRepository;
-    private $configurableProduct;
+
+    /**
+     * @var array
+     */
+    private $configurableProductSku = [];
+
+    /**
+     * @var AttributeAdapter
+     */
     private $attributeAdapter;
+
+    /**
+     * @var AttributeRepository
+     */
     private $attributeRepository;
 
+    /**
+     * @var ProductFactory
+     */
+    private $productFactory;
+
+    /**
+     * @var Entity
+     */
+    private $eavEntity;
+
+    /**
+     * @var AttributeSet
+     */
+    private $attributeSet;
+
+    /**
+     * Configurable product constructor.
+     *
+     * @param ProductRepository $productRepository
+     * @param ProductFactory $productFactory
+     * @param Entity $eavEntity
+     * @param AttributeAdapter $attributeAdapter
+     * @param AttributeRepository $attributeRepository
+     * @param AttributeSet $attributeSet
+     */
     public function __construct(
         ProductRepository $productRepository,
         ProductFactory $productFactory,
@@ -52,14 +99,23 @@ class Configurable extends Adapter
         $sku = $productData['General']['SKU'];
 
         if (isset($productData['Configurable Attributes'])) {
-            $this->configurableProduct = $product;
+            $this->configurableProductSku[] = $sku;
+            $this->configurableAttributes[$sku] = [];
+
             $configurableAttributes = explode(",", $productData['Configurable Attributes']);
 
             foreach($configurableAttributes as $configurableAttribute) {
-                $this->configurableAttributes[] = trim($configurableAttribute);
+                $this->configurableAttributes[$sku][] = trim($configurableAttribute);
             }
         } else {
-            $this->associations[] = $productData['General']['SKU'];
+            $configurableSku = $this->getConfigurableSku($productData);
+
+            if ($configurableSku) {
+                if (!isset($this->associations[$configurableSku])) {
+                    $this->associations[$configurableSku] = [];
+                }
+                $this->associations[$configurableSku][] = $sku;
+            }
         }
     }
 
@@ -69,38 +125,42 @@ class Configurable extends Adapter
      */
     public function afterDataProcess()
     {
-        $configurableAttributeIds = [];
+        foreach($this->configurableProductSku as $configurableProductSku)
+        {
+            $configurableAttributeIds = [];
+            $configurableProduct = $this->productRepository->get($configurableProductSku);
 
-        foreach ($this->configurableAttributes as $attributeCode) {
-            $attribute = $this->attributeRepository->get(4, $attributeCode);
-            $configurableAttributeIds[] = $attribute->getAttributeId();
-        }
-
-        if ($configurableAttributeIds) {
-
-            $this->configurableProduct->getTypeInstance()->setUsedProductAttributeIds(
-                $configurableAttributeIds,
-                $this->configurableProduct
-            );
-
-            $configurableAttributesData = $this->configurableProduct->getTypeInstance()->getConfigurableAttributesAsArray(
-                $this->configurableProduct
-            );
-
-            $this->configurableProduct->setCanSaveConfigurableAttributes(true);
-            $this->configurableProduct->setConfigurableAttributesData($configurableAttributesData);
-
-            $simpleProductIds = [];
-            $product = $this->productFactory->create();
-
-            foreach ($this->associations as $simpleProductSku) {
-                if ($productId = $product->getIdBySku($simpleProductSku)) {
-                    $simpleProductIds[] = $productId;
-                }
+            foreach ($this->configurableAttributes[$configurableProductSku] as $attributeCode) {
+                $attribute = $this->attributeRepository->get($this->getEntityTypeId(), $attributeCode);
+                $configurableAttributeIds[] = $attribute->getAttributeId();
             }
 
-            $this->configurableProduct->setAssociatedProductIds($simpleProductIds);
-            $this->productRepository->save($this->configurableProduct);
+            if ($configurableAttributeIds) {
+
+                $configurableProduct->getTypeInstance()->setUsedProductAttributeIds(
+                    $configurableAttributeIds,
+                    $configurableProduct
+                );
+
+                $configurableAttributesData = $configurableProduct->getTypeInstance()->getConfigurableAttributesAsArray(
+                    $configurableProduct
+                );
+
+                $configurableProduct->setCanSaveConfigurableAttributes(true);
+                $configurableProduct->setConfigurableAttributesData($configurableAttributesData);
+
+                $simpleProductIds = [];
+                $product = $this->productFactory->create();
+
+                foreach ($this->associations[$configurableProductSku] as $simpleProductSku) {
+                    if ($productId = $product->getIdBySku($simpleProductSku)) {
+                        $simpleProductIds[] = $productId;
+                    }
+                }
+
+                $configurableProduct->setAssociatedProductIds($simpleProductIds);
+                $this->productRepository->save($configurableProduct);
+            }
         }
     }
 
@@ -112,5 +172,22 @@ class Configurable extends Adapter
     private function getEntityTypeId()
     {
         return $this->eavEntity->setType(Product::ENTITY)->getTypeId();
+    }
+
+    /**
+     * @param $productData
+     * @return string
+     */
+    private function getConfigurableSku($productData)
+    {
+        $configurableSku = '';
+
+        foreach($productData['Additional Attributes']['Attributes'] as $attribute) {
+            if ($attribute['Attribute']['Name'] == self::CONFIGURABLE_ATTRIBUTE_NAME) {
+                $configurableSku = trim($attribute['Attribute']['Value']);
+            }
+        }
+
+        return $configurableSku;
     }
 }
